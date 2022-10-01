767b82d96c5b5f72d4f11fbfa172666dff384534
rkran <rkranjith03@gmail.com>
2022-10-01 18:53:57 +0530

    Selenium to bs4

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import redis
 from rq import Queue
 
 
-os.environ["SECRET_KEY"]="38493290320383843"
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
@@ -154,8 +154,8 @@ def logout():
 def add():
     form=AddForm()
     if form.validate_on_submit():
-        product=Amazon_Price(url=form.item_url.data)
         print(form.item_url.data)
+        product=Amazon_Price(url=form.item_url.data)
         product.get_add()
         product_price = product.price
         price = product_price
